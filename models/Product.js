cb5faaf05375454e1d19ea262b04e6c108ba7071
AlyLopez02 <alylopez0217@gmail.com>
2022-06-27 01:29:23 -0400

    finished server code and some models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,36 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true  //I am unsure if this is correct but I did find it on the sequelize documentation
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // REFERENCE THE CATEGORY MODEL'S ID
+    }
   },
   {
     sequelize,
